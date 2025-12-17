301f61d43c1a097bd647ab4c8c6559624f12454e
jluc9234 <dogpoundreject1@gmail.com>
2025-12-17 14:20:18 -0700

    hhjhj

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ minHeight: "100vh", backgroundColor: "#f9fafb" }}>
       <MockupCreator />
     </div>
   );
